c520e4cdc8f5137aca07c8f46ab22464a2854506
João <joaovitorsenac7817@gmail.com>
2024-05-06 14:33:55 -0300

    feat: Adicionar funcionalidade de Accordion para dt/dd

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,45 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('ativo');
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+  
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+  
+      tabContent[index].classList.add('ativo');
+    }
+  
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      })
     });
-
-    tabContent[index].classList.add('ativo');
   }
-
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    })
-  });
 }
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const classAtivo = 'ativo';
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(classAtivo);
+    accordionList[0].nextElementSibling.classList.add(classAtivo)
+    
+    function activeAccordion() {
+      this.classList.toggle(classAtivo);
+      this.nextElementSibling.classList.toggle(classAtivo);
+    }
+    
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+  
+}
+initAccordion();
