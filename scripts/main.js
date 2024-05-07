7afadbe1f31b7f406fa5ad7ab16783d2a9271cb1
João <joaovitorsenac7817@gmail.com>
2024-05-07 13:36:28 -0300

    feat: create scroll animation

--- scripts/main.js
+++ scripts/main.js
@@ -22,6 +22,7 @@ function initTabNav() {
 }
 initTabNav();
 
+
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
   const classAtivo = 'ativo';
@@ -43,3 +44,53 @@ function initAccordion() {
   
 }
 initAccordion();
+
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
+
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  const windowMetade = window.innerHeight * 0.6;
+  
+  if (sections.length) {
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        }
+      });
+    }
+    
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
